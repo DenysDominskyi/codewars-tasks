7dcf2cfb3e7d62404a8ab219cb533706737f933e
DenysDominskyi <den4ikdenis9@gmail.com>
2024-01-04 18:06:34 +0100

    task#4

--- index.js
+++ index.js
@@ -61,3 +61,45 @@ function digPow(n, p){
   newArr = newArr.reduce((a, b) => a + b, 0)
   return Number.isInteger(newArr / n) ? newArr / n : -1
 }
+
+// ----------------------------------------------------------------------------------------------------------
+
+// 4)-----Tribonacci Sequence-----
+
+// Well met with Fibonacci bigger brother, AKA Tribonacci.
+// As the name may already reveal, it works basically like a Fibonacci, but summing the last 3 (instead of 2) 
+// numbers of the sequence to generate the next. And, worse part of it, regrettably I won't get to hear non-native Italian speakers trying to pronounce it :(
+// So, if we are to start our Tribonacci sequence with [1, 1, 1] as a starting input (AKA signature), 
+// we have this sequence:
+// [1, 1 ,1, 3, 5, 9, 17, 31, ...]
+// But what if we started with [0, 0, 1] as a signature? As starting with [0, 1] instead of [1, 1] basically 
+// shifts the common Fibonacci sequence by once place, you may be tempted to think that we would get the same sequence shifted by 2 places, but that is not the case and we would get:
+// [0, 0, 1, 1, 2, 4, 7, 13, 24, ...]
+// Well, you may have guessed it by now, but to be clear: you need to create a fibonacci function that given 
+// a signature array/list, returns the first n elements - signature included of the so seeded sequence.
+// Signature will always contain 3 numbers; n will always be a non-negative number; if n == 0, then return 
+// an empty array (except in C return NULL) and be ready for anything else which is not clearly specified ;)
+
+
+function tribonacci(signature,n){
+  // If n is 0, return an empty array
+  if(n === 0) return []
+  // If n is less than or equal to 3, return the first n elements of the signature
+  if(n <= 3) return signature.slice(0, n)
+  // Create a copy of the signature array
+  const tribonacciSequence = [...signature]
+  // Iterate starting from the fourth element up to the nth element
+  for (let i = 3; i < n; i++) {
+    // Calculate the next element in the sequence by summing the last three elements
+    const nextElement = tribonacciSequence[i - 1] + 
+      tribonacciSequence[i - 2] + tribonacciSequence[i - 3]
+    // Push the next element to the tribonacciSequence array
+      tribonacciSequence.push(nextElement)
+  }
+  // Return the resulting Tribonacci sequence
+  return tribonacciSequence
+}
+
+// ----------------------------------------------------------------------------------------------------------
+
+// 5)-----  -----
