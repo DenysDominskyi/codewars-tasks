0c10cc4291b7f2b73c82e5714a455e45bb8c2f1c
DenysDominskyi <den4ikdenis9@gmail.com>
2024-01-15 18:19:21 +0100

    makeBeautifulScreen

---
+++ mouseMoving.js
@@ -0,0 +1,15 @@
+const cursor = document.querySelector('.interactive')
+
+document.addEventListener('mousemove', (e) => {
+    let x = e.pageX
+    let y = e.pageY
+    cursor.style.top = `${y}px`
+    cursor.style.left = `${x}px`
+    cursor.style.display = 'block'
+})
+
+document.addEventListener('mouseout', () => {
+    cursor.style.display = 'none'
+})
+
+console.log(cursor.style.transform)
