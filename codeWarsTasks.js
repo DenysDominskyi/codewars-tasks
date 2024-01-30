b2f9547bcb4c293d15eb9bfc1deb9ed210393cb2
DenysDominskyi <den4ikdenis9@gmail.com>
2024-01-30 19:39:51 +0100

    task#19

--- codeWarsTasks.js
+++ codeWarsTasks.js
@@ -635,11 +635,34 @@ function dirReduc(arr){
 
 // _______________________________________________________________________________________________________________
   
-// 19)-----  -----
+// 19)----- Calculating with Functions -----
+const zero = (operation) => (operation ? operation(0) : 0);
+const one = (operation) => (operation ? operation(1) : 1);
+const two = (operation) => (operation ? operation(2) : 2);
+const three = (operation) => (operation ? operation(3) : 3);
+const four = (operation) => (operation ? operation(4) : 4);
+const five = (operation) => (operation ? operation(5) : 5);
+const six = (operation) => (operation ? operation(6) : 6);
+const seven = (operation) => (operation ? operation(7) : 7);
+const eight = (operation) => (operation ? operation(8) : 8);
+const nine = (operation) => (operation ? operation(9) : 9);
+
+const plus = (x) => (y) => y + x;
+const minus = (x) => (y) => y - x;
+const times = (x) => (y) => y * x;
+const dividedBy = (x) => (y) => Math.floor(y / x);
+// seven(times(five())); // must return 35
+// four(plus(nine())); // must return 13
+// eight(minus(three())); // must return 5
+// six(dividedBy(two())); // must return 3
+
+// _______________________________________________________________________________________________________________
+  
+// 20)-----  -----
 
 
 
 
 // _______________________________________________________________________________________________________________
   
-// 20)-----  -----
+// 21)-----  -----
