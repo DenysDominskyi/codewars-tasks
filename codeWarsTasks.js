65b9bd1cc9610bb12c9697aa3f91f8a7bb03aa42
DenysDominskyi <den4ikdenis9@gmail.com>
2024-02-01 16:49:21 +0100

    task#21

--- codeWarsTasks.js
+++ codeWarsTasks.js
@@ -667,10 +667,23 @@ function pigIt(str){
 
 // _______________________________________________________________________________________________________________
   
-// 21)-----  -----
+// 21)----- Human Readable Time -----
+function humanReadable (seconds) {
+  if (seconds < 0 || seconds > 359999) {
+    return "Invalid input";
+  }
 
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  const remainingSeconds = seconds % 60;
 
+  const formattedHours = String(hours).padStart(2, '0');
+  const formattedMinutes = String(minutes).padStart(2, '0');
+  const formattedSeconds = String(remainingSeconds).padStart(2, '0');
 
+  return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+}
+// console.log(humanReadable(9999))
 
 
 // _______________________________________________________________________________________________________________
